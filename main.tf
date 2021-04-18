fbedc26bffe5285d6a6ad9f8e5788fc32737db4e
@slovacus <slovacus@gmail.com>
2021-04-18 11:03:18 -0500

    chore: implement output name (#5)

--- main.tf
+++ main.tf
@@ -5,20 +5,23 @@ locals {
 
   input = {
     agree_terms = try(var.agree_terms, local.defaults.agree_terms)
+    name        = var.name
   }
 
   generated = {
     agree_terms = local.input.agree_terms
+    name        = local.input.name
   }
 
   outputs = {
     agree_terms = local.generated.agree_terms
+    name        = local.generated.name
   }
 }
 
 # Create an organization
 resource "sentry_organization" "this" {
-  name        = var.name
+  name        = local.outputs.name
   slug        = var.slug
   agree_terms = local.outputs.agree_terms
 }
