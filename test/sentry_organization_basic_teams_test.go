2c2a28303ccfbed9922109099b5031dd2a3bd0e7
@slovacus <slovacus@gmail.com>
2021-04-18 21:20:55 -0500

    test: implement sentry make teams (#7)

---
+++ test/sentry_organization_basic_teams_test.go
@@ -0,0 +1,42 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBasicTeamsSuccess(t *testing.T) {
+	t.Parallel()
+
+	name := "test-organization"
+	teams := []map[string]interface{}{
+		{
+			"name": "team1",
+		},
+		{
+			"name": "team2",
+		},
+	}
+
+	terraformOptions := &terraform.Options{
+		// The path to where your Terraform code is located
+		TerraformDir: "sentry-organization-basic-teams",
+		Upgrade:      true,
+		Vars: map[string]interface{}{
+			"name":  name,
+			"teams": teams,
+		},
+	}
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terraformOptions)
+
+	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformOptions)
+	outputOrganization := terraform.Output(t, terraformOptions, "instance")
+	outputName := terraform.Output(t, terraformOptions, "name")
+	assert.NotEmpty(t, outputOrganization, outputOrganization)
+	assert.NotEmpty(t, outputName, outputName)
+}
