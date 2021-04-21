32d6e44c07388f2b6bac502f77402b51fb20eac4
@slovacus <slovacus@gmail.com>
2021-04-21 14:14:26 -0500

    build: generate version minor (0.2.0)

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,5 +1,5 @@
 module "main" {
-  source      = "git://github.com/hadenlabs/terraform-sentry-organization.git?ref=0.1.0"
+  source      = "git://github.com/hadenlabs/terraform-sentry-organization.git?ref=0.2.0"
   name        = var.name
   slug        = var.slug
   agree_terms = var.agree_terms
