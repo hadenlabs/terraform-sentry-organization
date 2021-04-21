8fc53ac9b5e18b564e0e5d1d639fbb3879babec5
@slovacus <slovacus@gmail.com>
2021-04-21 13:55:37 -0500

    test: implement validation output teams (#10)

---
+++ test/sentry-organization-output-teams/outputs.tf
@@ -0,0 +1,19 @@
+output "instance" {
+  description = "output instance organization"
+  value       = module.main.instance
+}
+
+output "name" {
+  description = "output name organization"
+  value       = module.main.name
+}
+
+output "id" {
+  description = "output id organization"
+  value       = module.main.id
+}
+
+output "teams" {
+  description = "output teams of organization"
+  value       = module.main.teams
+}
